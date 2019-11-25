350788034430d4010b4dd4be01dfd37dbef4ce50
Jos Muysers <jos@muysers.nl>
2019-11-25 11:15:01 +0100

    deleted unused functions

--- api/utils.go
+++ api/utils.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"compress/gzip"
 	"encoding/json"
-	"log"
 	"net/http"
 	"regexp"
 	"strconv"
@@ -80,77 +79,11 @@ func compress(inp []byte) ([]byte, error) {
 	return []byte(buf.String()), nil
 }
 
-func getColsWithValues(c db.Conn, dbName string, tblName string, r *http.Request) []db.Column {
-	//TODO: delet this function?
-	cols, err := c.GetColumns(dbName, tblName)
-	data, err := getRequestData(r)
-	if err != nil {
-		log.Println("REST: ERROR: POST:", dbName, tblName, err)
-	}
-
-	//set column values
-	values2columns(&cols, data)
-	return cols
-}
-
-func values2columns(cols *[]db.Column, values map[string]interface{}) {
-	//TODO: used in more than getColsWithValues?
-	for key, value := range values {
-		index := findColIndex(key, *cols)
-		if index > -1 {
-			(*cols)[index].Value = db.Escape(value.(string))
-		}
-	}
-}
-
 func findColIndex(field string, cols []db.Column) int {
-	//TODO: delete this function?
 	for index, col := range cols {
 		if strings.ToLower(col.Name) == strings.ToLower(field) {
 			return index
 		}
 	}
 	return -1
-}
-
-func cols2json(table string, cols []db.Column) ([]byte, error) {
-	//TODO: delete this function?
-	var ret map[string]interface{}
-	ret = make(map[string]interface{})
-	ret["type"] = table
-	for _, col := range cols {
-		ret[col.Name] = col.Value
-	}
-	json, err := json.Marshal(ret)
-	if err != nil {
-		return []byte(""), err
-	}
-	return json, nil
-}
-
-//getRequestData get data from post request
-func getRequestData(req *http.Request) (map[string]interface{}, error) {
-	//TODO: delete this function?
-	err := req.ParseForm()
-	if err != nil {
-		return make(map[string]interface{}), err
-	}
-	res := make(map[string]interface{})
-	for k, v := range req.Form {
-		res[k] = strings.Join(v, "")
-	}
-	return res, nil
-}
-
-func setAutoIncColumn(id int, cols []db.Column) []db.Column {
-	//TODO: delete this function?
-	//fmt.Println("DEBUG:setAutoIncColumn")
-	for index, col := range cols {
-		//TODO: put AutoIncrement boolean in column type
-		if strings.Contains(col.Type, "int") && col.PrimaryKey == true {
-			//fmt.Println("DEBUG:found", col.Field)
-			cols[index].Value = id
-		}
-	}
-	return cols
 }
