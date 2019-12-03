2c2b44f84f99abac5ab1b323027c4e86474f9a7c
Jos Muysers <jos@muysers.nl>
2019-12-03 12:10:13 +0100

    fixed comma index name

--- db/utils.go
+++ db/utils.go
@@ -194,6 +194,6 @@ func SortTablesByForeignKey(tbls []Table) {
 //SetIndexName sets index name if it is empty or if its the same as Columns
 func SetIndexName(schemaName, tableName string, index *Index) {
 	if len(index.Name) == 0 || index.Name == index.Columns {
-		index.Name = schemaName + "_" + tableName + "_" + strings.Replace(index.Columns, ", ", "_", -1) + "_index"
+		index.Name = schemaName + "_" + tableName + "_" + strings.Replace(strings.Replace(index.Columns, ", ", "_", -1), ",", "_", -1) + "_index"
 	}
 }
