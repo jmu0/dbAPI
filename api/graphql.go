a2180009a2040fc405683a33f347e1fd9d30ca41
Jos Muysers <jos@muysers.nl>
2020-01-15 08:21:37 +0100

    lock queryCache

--- api/graphql.go
+++ api/graphql.go
@@ -586,11 +586,11 @@ func args2cols(args map[string]interface{}, cols []db.Column) {
 }
 
 func deleteFromQueryCache(schemaName, tableName string) {
+	mutex.Lock()
 	for q := range queryCache {
 		if strings.Contains(q, schemaName) && strings.Contains(q, tableName) {
-			mutex.Lock()
 			delete(queryCache, q)
-			mutex.Unlock()
 		}
 	}
+	mutex.Unlock()
 }
